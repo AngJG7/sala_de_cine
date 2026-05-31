59c87fc78f612c84a17ab51e5b12ec25134e8cc5
Sebastian Murillo <sebasmg0204@gmail.com>
2026-05-31 06:52:53 -0500

    Update AppCine.py

--- AppCine.py
+++ AppCine.py
@@ -91,7 +91,7 @@ class AppCine:
             print(f'''
 ☆゜・。。・゜Menú Administrador ゜・。。・゜★
                       
-   1) Crear nueva película
+    1) Crear nueva película
     2) Modificar estado película
     3) Crear nueva sala
     4) Consultar detalle de la película
@@ -550,6 +550,28 @@ class AppCine:
         input("\nPresione Enter para regresar al menú...")  
 
     def consultar_recaudo_complejo(self):
+        print("\n━━━━━━✧ Consultar recaudo de una sala ✧━━━━━━")
+        
+        try:
+            id_sala_buscar = int(input("Identificador de la sala: "))
+            
+            sala_encontrada = self.complejo.buscar_sala(id_sala_buscar)
+            if sala_encontrada == None:
+                print(f"No se encontró ninguna sala con ese identificador.")
+                input("\nPresione Enter para continuar...")
+                return
+                
+            total_recaudo = sala_encontrada.calcular_recaudado()
+            
+            print(f"\nResumen de Recaudo - Sala {id_sala_buscar}:")
+            print(f"Total recaudado: ${total_recaudo:,.2f}")
+            
+        except ValueError:
+            print("\nError. El identificador de la sala debe ser un número entero.")
+            
+        input("\nPresione Enter para regresar al menú...")  
+
+    def consultar_recaudo_complejo(self):
         print("\n━━━━━━✧ Consultar recaudo del complejo ✧━━━━━━")
         
         if self.complejo.cantidad_salas == 0:
@@ -567,12 +589,13 @@ class AppCine:
             print(f"Sala {sala_actual.identificador:<5} | Recaudo: ${recaudo_sala:,.2f}")
             i += 1
             
-        total_general = self.complejo.calcular_recaudo()
+        total_general = self.complejo.calcular_recaudado()
         
         print("─" * 45)
         print(f"TOTAL GENERAL DEL COMPLEJO: ${total_general:,.2f}")
             
         input("\nPresione Enter para regresar al menú...")
+
 
 
 
